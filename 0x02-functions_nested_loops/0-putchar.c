be8b7c9daaf5dd9d183ffa6fa71cafc8c536f716
sikeola <jesupemioluwadamilola12@gmail.com>
2022-09-14 07:26:18 -0700

    hello

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,37 +3,36 @@
 
 
 /**
- *
- *  * main -  prints _putchar, followed by a new line
- *
- *   * Return: Always 0 (Success)
- *
- *    */
+
+ * main -  prints _putchar, followed by a new line
+
+ * Return: Always 0 (Success)
+
+ */
 
 int main(void)
 
 {
 
-		_putchar('_');
+	_putchar('_');
 
-			_putchar('p');
+	_putchar('p');
 
-				_putchar('u');
+	_putchar('u');
 
-					_putchar('t');
+	_putchar('t');
 
-						_putchar('c');
+	_putchar('c');
 
-							_putchar('h');
+	_putchar('h');
 
-								_putchar('a');
+	_putchar('a');
 
-									_putchar('r');
+	_putchar('r');
 
-										_putchar('\n');
+	_putchar('\n');
 
 
 
-											return (0);
-
+	return (0);
 }
